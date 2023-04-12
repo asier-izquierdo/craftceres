cd920969d4c0babd5fb5bcfbd6983c91130f6f84
Asier Izquierdo <80033030+asier-izquierdo@users.noreply.github.com>
2023-04-12 16:52:00 +0200

    fixed report arguments

--- updater.sh
+++ updater.sh
@@ -18,7 +18,7 @@ version=$(curl $PAPER_DOWNLOAD_URL | grep -Eo $MC_VERSION_REGEX | sort -r | head
 latest=$(curl $PAPER_API_URL/versions/$version/builds/ | grep -Eo '"build":\$BUILD_NUMBER_REGEX' | sort -r | head -1 | cut -d: -f 2)
 
 server_starter() {
-        handler INFO 0 "Restarting the server wit the $1 build..."
+        handler "INFO" 0 "Restarting the server wit the $1 build..."
         sleep 20
         tmux send-keys -t $tmuxsession:0 "java -Xmx2G -Xms16G -jar $path/paper-$version-$2.jar nogui" Enter
 }
@@ -52,32 +52,32 @@ handler() {
 
 check_dependency() {
   command -v "$1" >/dev/null 2>&1 || {
-    handler 7 ERROR "Missing dependency $1."
+    handler 7 "ERROR" "Missing dependency $1."
   }
 }
-
-# Create the log file if it doesn't already exist on the specified path
-if [ ! -f $logfile ]
-then    echo "[INFO: 0  ($(date))] Created log for the PaperMC updater script." > $logfile
-fi
 
 check_dependency "curl"
 check_dependency "tmux"
 check_dependency "java"
 check_dependency "wget"
 
-handler INFO 0 "Starting updater execution..."
+# Create the log file if it doesn't already exist on the specified path
+if [ ! -f $logfile ]
+then    echo "[INFO: 0  ($(date))] Created log for the PaperMC updater script." > $logfile
+fi
+
+handler "INFO" 0 "Starting updater execution..."
 
 # Create the archive directory to store the previously used PaperMC build if it doesn't already exist
 if [ ! -d $archive ]
 then
-        handler INFO 0 "No archives directory found. Creating it..."
+        handler "INFO" 0 "No archives directory found. Creating it..."
 
         mkdir $archive 
 
         if [ $? -ne 0 ]
-        then    handler ERROR 2 "Could not create the archives directory. It is necessary for archiving previously working .jars in a tidied manener."
-        else    handler INFO 0 "Created directory for archiving the latest working PaperMC .jar"
+        then    handler "ERROR" 2 "Could not create the archives directory. It is necessary for archiving previously working .jars in a tidied manener."
+        else    handler "INFO" 0 "Created directory for archiving the latest working PaperMC .jar"
         fi
 
 fi
@@ -95,14 +95,14 @@ then
                         wget https://api.papermc.io/v2/projects/paper/versions/$version/builds/$latest/downloads/paper-$version-$latest.jar -P $path
 
                         if [ $? -ne 0 ]
-                        then    handler ERROR 5 "The latest PaperMC build could not be downloaded."
+                        then    handler "ERROR" 5 "The latest PaperMC build could not be downloaded."
                         else    mv $current $archive
                         fi
 
-                else    handler ERROR 3 "The server failed to stop."
+                else    handler "ERROR" 3 "The server failed to stop."
                 fi
 
-        else    handler WARNING 1 "The PaperMC server was not running." >> $logfile
+        else    handler "WARNING" 1 "The PaperMC server was not running." >> $logfile
         fi
 
         if [ -f $path/paper-$version-$latest.jar ]
@@ -110,14 +110,14 @@ then
                 server_starter "latest" $latest
 
                 if [ -z "$(pidof java)" ]
-                then    handler ERROR 6 "The server failed to start using the latest PaperMC build."
+                then    handler "ERROR" 6 "The server failed to start using the latest PaperMC build."
                 fi
 
-        else    handler ERROR 4 "The latest PaperMC version could not be found."
+        else    handler "ERROR" 4 "The latest PaperMC version could not be found."
         fi
 
-        handler INFO 0 "The PaperMC server has successfully been updated and restarted."
+        handler "INFO" 0 "The PaperMC server has successfully been updated and restarted."
 
-else    handler INFO 0 "There were no updates for the server."
+else    handler "INFO" 0 "There were no updates for the server."
 
 fi
